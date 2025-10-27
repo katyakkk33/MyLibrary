005648ba4a52d9e16454cac6181a503e5fc5334b
Ddmyttroo <dimahalynovish@gmail.com>
2025-10-27 21:55:26 +0100

    Update Book.ts

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -8,5 +8,7 @@ export interface Book {
   isbn?: string | null;
   cover_url?: string | null;
   description?: string | null;
+  year_published?: number | null;
+  genre?: string | null;
 }
 export type NewBook = Omit<Book, 'id' | 'data_dodania'>;
